3380e0c38556d38ea0ac9686880b37715c72f2c4
Justin Hoang <justinhoang124@gmail.com>
2024-06-02 21:48:09 -0600

    Added comment about the default export

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,12 @@
 // Every component should be responsible for its own job
 // https://gist.githubusercontent.com/adrianhajdin/8a04faf5e52419ec93cd5329ffcb011e/raw/119ddf3380cecf2a3736afe8e4c83ee008093b6b/index.js
+
+// If I didn't mark Navbar as `export default Navbar` in
+// `src/components/Navbar.jsx`, I would have to do something like this to
+// specifically grabbed the named export, rather than grabbing the singular
+// default value and naming it myself
+// import { Navbar } from "./Navbar";
+
 import Navbar from "./Navbar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
